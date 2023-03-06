fc0729ec26c49068f7bc3fcd1cb4e89a756c786a
admin <admin@DESKTOP-8HAIHSV>
2023-03-06 13:43:24 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Hello, World!");
+int[,] masd = new int[5, 5];
+for(int i = 0;i < 5; i++)
+{
+    for(int j = 0;j<5;j++)
+    {
+        if(i+j == 4)
+        masd[i, j] = i + 1;
+        Console.Write(masd[i, j].ToString() +' ');
+    }
+    Console.WriteLine();
+}    
